0ad41e7db0bb32b2421d7f8e1865ec7a383fc7e6
Derek Saffe <desaff@gmail.com>
2020-09-27 15:25:55 -0300

    fixed layout for deploy

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -8,17 +8,12 @@ import '../styles/layout.css'
 import styles from './layout.module.css'
 
 const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => {
-  console.log(location.pathname)
   return (
     <>
       <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
-      <StyledLayout path={location.pathname} className={styles.content}>{children}</StyledLayout>
+      <Layout className={styles.content}>{children}</Layout>
     </>
   )
 }
-
-const StyledLayout = styled.main`
-  background: ${({path}) => (path === '/ar/' ? '#FFEAC1' : '#FFEAC1')}
-`
 
 export default Layout
