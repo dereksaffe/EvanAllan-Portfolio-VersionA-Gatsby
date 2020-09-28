d57824ac60e7b9f5719c9a6eaa3d3ce42cfcc7b9
Derek Saffe <desaff@gmail.com>
2020-09-28 11:47:55 -0300

    Revert "fixed layout for deploy"
    This reverts commit 0ad41e7db0bb32b2421d7f8e1865ec7a383fc7e6.

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -8,12 +8,17 @@ import '../styles/layout.css'
 import styles from './layout.module.css'
 
 const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => {
+  console.log(location.pathname)
   return (
     <>
       <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
-      <Layout className={styles.content}>{children}</Layout>
+      <StyledLayout path={location.pathname} className={styles.content}>{children}</StyledLayout>
     </>
   )
 }
+
+const StyledLayout = styled.main`
+  background: ${({path}) => (path === '/ar/' ? '#FFEAC1' : '#FFEAC1')}
+`
 
 export default Layout
